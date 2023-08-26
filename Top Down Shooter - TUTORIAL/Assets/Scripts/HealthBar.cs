3f39c88f38cd7a3c3f31c9024f45d9aa2f9c7de8
Isharma-mi <ishaan0sharma@gmail.com>
2023-08-26 15:05:04 -0400

    Health bar gradient
    Added gradient styling to health bar
    - Switches from green, yellow, and red based off how much health left
    - Need to add health bar and health functionality for enemy
    - Need to have melee enemy move towards player

--- Top Down Shooter - TUTORIAL/Assets/Scripts/HealthBar.cs
+++ Top Down Shooter - TUTORIAL/Assets/Scripts/HealthBar.cs
@@ -7,17 +7,25 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider healthBarSlider;
+    public Gradient healthBarGradient;
+    public Image fill;
 
     // Sets max value of health bar and makes health equal to it
     public void SetMaxHealth(int maxHealth)
     {
         healthBarSlider.maxValue = maxHealth;
         healthBarSlider.value = maxHealth;
+
+        //Makes health bar green when full
+        fill.color = healthBarGradient.Evaluate(1.0f);
     }
 
     // Changes health value
     public void UpdateHealth(int health)
     {
         healthBarSlider.value = health;
+
+        // Sets color of healthbar based off how much health is left
+        fill.color = healthBarGradient.Evaluate(healthBarSlider.normalizedValue);
     }
 }
